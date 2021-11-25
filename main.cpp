419f6746366e01b7d1ee147004f3c6541ba9920b
muhalli2001 <muhalli2001@knights.ucf.edu>
2021-11-24 21:10:34 -0500

    cc

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 /*
-*  UCF COP3330 Fall 2021 Assignment 5 Solution
-*  Copyright 2021 Muhammad Alli
+*  UCF COP3330 Fall 2021 Assignment 5 Solution  Copyright 2021 Muhammad Alli
 */
 
 #include "std_lib_facilities.h"
